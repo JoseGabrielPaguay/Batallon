1e75cd7e61d5c67a9d74f51c312df71dc81d3fb6
Jose Gabriel <joseg.paguayi@uqvirtual.edu.co>
2025-04-10 15:36:49 -0500

    Delete untitled directory

--- untitled/src/main/java/co/edu/uniquindio/EstadoOperativo.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio;
-
-public enum EstadoOperativo {
-}
--- untitled/src/main/java/co/edu/uniquindio/Main.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio;
-public class Main {
-    public static void main(String[] args) {
-        Vehiculo newVehiculo=new TransporteTRopa("1001","2005",2005,1000,10);
-    }
-
-}
